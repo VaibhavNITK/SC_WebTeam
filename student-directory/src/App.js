42805fb7d6b0e114345b4ff9eb21703756eaaa42
Vaibhav <vaibhavagarwal693@gmail.com>
2023-06-28 04:52:17 +0530

    Task1

---
+++ student-directory/src/App.js
@@ -0,0 +1,12 @@
+
+// import './App.css';
+import StudentDirectory from "./StudentDirectory";
+function App() {
+  return (
+    <div className="App">
+      <StudentDirectory />
+    </div>
+  );
+}
+
+export default App;
